ca7f08b9f25c4b5fd4999ebb08b8fbed393e6141
Siddharth <apple@Apples-MacBook-Pro.local>
2020-03-25 16:47:22 +0530

    first commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,18 @@
+<style>
+  nav {
+    background-color: #fff;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.0625);
+    display: block;
+    height: 65px;
+    margin-bottom: 0;
+    padding: 0;
+    position: fixed;
+    -webkit-transition: all 0.2s ease;
+    -o-transition: all 0.2s ease;
+    transition: all 0.2s ease;
+    width: 100%;
+    z-index: 800;
+  }
+</style>
+
+<nav />
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+</script>
+
+<style>
+  main {
+    padding-top: 85px;
+    position: relative;
+    max-width: 56em;
+    margin: 0 auto;
+  }
+</style>
+
+<Nav />
+<main>
+  <slot />
+</main>
