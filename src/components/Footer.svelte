8b90c825c369c006ae057ef980ff621ab1c523dd
Siddharth <pandeysiddharth27@gmail.com>
2020-03-27 23:52:37 +0530

    Added noopener noreffer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -21,11 +21,12 @@
   <a
     href="https://www.facebook.com/pg/Covid19-100499308274742/services/?ref=page_internal"
     target="_blank"
+    rel="noopener noreferrer"
   >
     <img src="fb.png" alt="Fb" />
   </a>
   <div>Created and Maintained by</div>
-  <a href="https://github.com/Sidd27/covid-19-dashboard" target="_blank">
+  <a href="https://github.com/Sidd27/covid-19-dashboard" target="_blank" rel="noopener noreferrer">
     <img src="github.png" alt="github" />
   </a>
 </footer>
